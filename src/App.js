c5ae22f94c18dd71b4c17bfd78545a36c869153b
Andrew Acin <andrew_acin@hotmail.com>
2024-06-19 21:58:42 -0700

    added logos and text container

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './TopBar';
 import Background from './Background';
+import Banner from './Banner';
+import StoreListings from './StoreListings';
 
 
 
@@ -10,7 +12,12 @@ function App() {
     <div className="App">
       <Navbar />      
       <div className='background-div'>
-        <Background />
+        <Banner />
+        <Background />        
+      </div>
+      <div className='listings'>
+        <h4>Browse stores in Ls Vegas</h4>
+        <StoreListings />
       </div>
     </div>
     
