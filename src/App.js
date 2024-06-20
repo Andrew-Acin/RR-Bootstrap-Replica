e905fef6b88c905a5bf5d1a38bf6396ce1c31a14
Andrew Acin <andrew_acin@hotmail.com>
2024-06-19 20:57:48 -0700

    fixing up background image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './TopBar';
+import Background from './Background';
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Navbar />      
+      <div className='background-div'>
+        <Background />
+      </div>
     </div>
+    
   );
 }
+
+
 
 export default App;
